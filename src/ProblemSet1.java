10eda5f973ddaae976d927ea192e17f8dcad1271
Justin Latorre <jlatorre2000@gmail.com>
2018-09-20 18:28:58 -0400

    solved version

--- src/ProblemSet1.java
+++ src/ProblemSet1.java
@@ -6,22 +6,40 @@
  * explained (and demonstrated) in detail in the README file.
  */
 
-public class ProblemSet1 {	
-	
-	/**
-	 * Every Java program must have a main method, which is
-	 * where program execution begins.
-	 */
-	
-	public static void main(String[] args) {
-		System.out.println("/// EXERCISE 1 /////////////////////////////////////////////////\n");
-		
-		System.out.println("/// EXERCISE 2 /////////////////////////////////////////////////\n");
-		
-		System.out.println("/// EXERCISE 3 /////////////////////////////////////////////////\n");
-		
-		System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n");
-		
-		System.out.println("/// EXERCISE 5 /////////////////////////////////////////////////\n");
-	}
+public class ProblemSet1 {    
+    
+    /**
+     * Every Java program must have a <code>main</code> method, which is
+     * where program execution begins.
+     */
+    
+    public static void main(String[] args) {
+        System.out.println("/// EXERCISE 1 /////////////////////////////////////////////////\n");
+        System.out.println("   ##    ######  ######## ########");
+        System.out.println("  #  #   #     # #        #");
+        System.out.println(    " #    #  #     # #        #");
+        System.out.println("######## ######  #        ########"); 
+        System.out.println("#      # #       #               #");
+        System.out.println("#      # #       #               #");
+        System.out.println("#      # #       ######## ########");
+        
+        System.out.println("/// EXERCISE 2 /////////////////////////////////////////////////\n");
+
+        System.out.println("  /\\     /\\     /\\");
+        System.out.println(" /  \\   /  \\   /  \\");
+        System.out.println("/    \\ /    \\ /    \\");
+        System.out.println("\\    / \\    / \\    /");
+        System.out.println(" \\  /   \\  /   \\  /");
+        System.out.println("  \\/     \\/     \\/");
+        System.out.println("/// EXERCISE 3 /////////////////////////////////////////////////\n");
+        System.out.print("   \\\\\\\\\\ \n");
+        System.out.print("  +\"\"\"\"\"+ \n");    
+        System.out.print("(|  0 0  |) \n");
+        System.out.print(" |   ^   | \n");
+        System.out.print(" |  '-'  | \n");
+        System.out.print("  +-----+ \n");
+        System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n H E L L O \n E       A \n L       P \n L       C \n O A P C S \n");
+        System.out.println("/// EXERCISE 5 /////////////////////////////////////////////////\n \"The answer's quite simple,\" the professor said. \"Just think outside the box!\"");
+    }
 }
+
